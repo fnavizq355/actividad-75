3a6e6eb4505a51bed56c4eba769f1c1f33b1838b
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-06-15 08:38:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Green))
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 5) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
         maqueen.motorStop(maqueen.Motors.All)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
